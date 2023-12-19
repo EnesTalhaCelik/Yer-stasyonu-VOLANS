60ca38c91ea411ec86c3930ada334962f773c0b7
Enes Talha Çelik <147333492+EnesTalhaCelik@users.noreply.github.com>
2023-12-19 15:12:10 +0300

    Update veriler.java
    veriler sınıfına seri haberleşmede kullanacağımız metotları ekledim

--- VolansYerIstasyonu/src/main/java/com/volans/volansyeristasyonu/veriler.java
+++ VolansYerIstasyonu/src/main/java/com/volans/volansyeristasyonu/veriler.java
@@ -1,26 +1,177 @@
 package com.volans.volansyeristasyonu;
 
+import java.nio.ByteBuffer;
+
 public class veriler {
 
-    private static double jiroskopX;
-    private static double jiroskopY;
-    private static double jiroskopZ;
-    private static double ivmeX;
-    private static double ivmeY;
-    private static double ivmeZ;
-    private static double aci;
 
 
 
-    private static double gorevYukuBoylam;
-    private static double gorevYukuEnlem;
-    private static double gorevYukuGpsIrtifa;
-    private static double roketBoylam;
-    private static double roketEnlem;
-    private static double roketGpsIrtifa;
+    private static float jiroskopX;
+    private static float jiroskopY;
+    private static float jiroskopZ;
+    private static float ivmeX;
+    private static float ivmeY;
+    private static float ivmeZ=9.77779f; // test için bu verileri girdim
+    private static float aci = 98.85f;  // test için bu verileri girdim
+
+
+
+    private static float gorevYukuBoylam;
+    private static float gorevYukuEnlem;
+    private static float gorevYukuGpsIrtifa;
+    private static float roketBoylam;
+    private static float roketEnlem;
+    private static float roketGpsIrtifa; //gps verisinden gelen irtifa değeridir.
+    private static float basincIrtifa; //basınç sensöründen gelen irtifa değeridir
+
+    private static float kademeBoylam ;
+
+    private static float kademeEnlem;
+
+    private static float kademeGpsIrtifa;
 
     private static int durum ;
 
+    private static int crc;
+
+    private static int takimID;
+
+    private static int paketSayac;
+
+
+
+
+    private static byte[] Aktarilacakveriler = new byte[78]; //Hakem bilgisayarına göndereceğimiz veriler
+
+
+    public static float getJiroskopX() {
+        return jiroskopX;
+    }
+
+    public static void setJiroskopX(float jiroskopX) {
+        veriler.jiroskopX = jiroskopX;
+    }
+
+    public static float getJiroskopY() {
+        return jiroskopY;
+    }
+
+    public static void setJiroskopY(float jiroskopY) {
+        veriler.jiroskopY = jiroskopY;
+    }
+
+    public static float getJiroskopZ() {
+        return jiroskopZ;
+    }
+
+    public static void setJiroskopZ(float jiroskopZ) {
+        veriler.jiroskopZ = jiroskopZ;
+    }
+
+    public static float getIvmeX() {
+        return ivmeX;
+    }
+
+    public static void setIvmeX(float ivmeX) {
+        veriler.ivmeX = ivmeX;
+    }
+
+    public static float getIvmeY() {
+        return ivmeY;
+    }
+
+    public static void setIvmeY(float ivmeY) {
+        veriler.ivmeY = ivmeY;
+    }
+
+    public static float getIvmeZ() {
+        return ivmeZ;
+    }
+
+    public static void setIvmeZ(float ivmeZ) {
+        veriler.ivmeZ = ivmeZ;
+    }
+
+    public static float getAci() {
+        return aci;
+    }
+
+    public static void setAci(float aci) {
+        veriler.aci = aci;
+    }
+
+    public static float getGorevYukuBoylam() {
+        return gorevYukuBoylam;
+    }
+
+    public static void setGorevYukuBoylam(float gorevYukuBoylam) {
+        veriler.gorevYukuBoylam = gorevYukuBoylam;
+    }
+
+    public static float getGorevYukuEnlem() {
+        return gorevYukuEnlem;
+    }
+
+    public static void setGorevYukuEnlem(float gorevYukuEnlem) {
+        veriler.gorevYukuEnlem = gorevYukuEnlem;
+    }
+
+    public static float getGorevYukuGpsIrtifa() {
+        return gorevYukuGpsIrtifa;
+    }
+
+    public static void setGorevYukuGpsIrtifa(float gorevYukuGpsIrtifa) {
+        veriler.gorevYukuGpsIrtifa = gorevYukuGpsIrtifa;
+    }
+
+    public static float getRoketBoylam() {
+        return roketBoylam;
+    }
+
+    public static void setRoketBoylam(float roketBoylam) {
+        veriler.roketBoylam = roketBoylam;
+    }
+
+    public static float getRoketEnlem() {
+        return roketEnlem;
+    }
+
+    public static void setRoketEnlem(float roketEnlem) {
+        veriler.roketEnlem = roketEnlem;
+    }
+
+    public static float getRoketGpsIrtifa() {
+        return roketGpsIrtifa;
+    }
+
+    public static void setRoketGpsIrtifa(float roketGpsIrtifa) {
+        veriler.roketGpsIrtifa = roketGpsIrtifa;
+    }
+
+    public static float getKademeBoylam() {
+        return kademeBoylam;
+    }
+
+    public static void setKademeBoylam(float kademeBoylam) {
+        veriler.kademeBoylam = kademeBoylam;
+    }
+
+    public static float getKademeEnlem() {
+        return kademeEnlem;
+    }
+
+    public static void setKademeEnlem(float kademeEnlem) {
+        veriler.kademeEnlem = kademeEnlem;
+    }
+
+    public static float getKademeGpsIrtifa() {
+        return kademeGpsIrtifa;
+    }
+
+    public static void setKademeGpsIrtifa(float kademeGpsIrtifa) {
+        veriler.kademeGpsIrtifa = kademeGpsIrtifa;
+    }
 
     public static int getDurum() {
         return durum;
@@ -30,108 +181,117 @@ public class veriler {
         veriler.durum = durum;
     }
 
-
-    public static double getJiroskopX() {
-        return jiroskopX;
+    public static byte[] getAktarilacakveriler() {
+        return Aktarilacakveriler;
     }
 
-    public static void setJiroskopX(double jiroskopX) {
-        veriler.jiroskopX = jiroskopX;
+    public static void setAktarilacakveriler(byte aktarilacakveriler,int sira) {
+        Aktarilacakveriler[sira] = aktarilacakveriler;
+    }
+    public static void setAktarilacakverilerINIT() {
+        Aktarilacakveriler[0] = (byte) 255;
+        Aktarilacakveriler[1] = (byte) 255;
+        Aktarilacakveriler[2] = (byte) 84;
+        Aktarilacakveriler[3] = (byte) 82;
+        Aktarilacakveriler[76] = (byte) 13;
+        Aktarilacakveriler[77] = (byte) 10;
     }
 
-    public static double getJiroskopY() {
-        return jiroskopY;
+    public static float getBasincIrtifa() {
+        return basincIrtifa;
     }
 
-    public static void setJiroskopY(double jiroskopY) {
-        veriler.jiroskopY = jiroskopY;
+    public static void setBasincIrtifa(float basincIrtifa) {
+        veriler.basincIrtifa = basincIrtifa;
     }
 
-    public static double getJiroskopZ() {
-        return jiroskopZ;
+
+
+    public static void arrayFloatToByte(){
+        int sayac = 4; //yazmak istediğimiz veriler arrayin 6.hanesine takbul ediyor.O noktadan itibaren yazmaya başlıyoruz.
+
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        ByteBuffer bufferINT = ByteBuffer.allocate(Integer.BYTES);
+
+        bufferINT.putInt(takimID);
+        System.arraycopy(bufferINT.array(), 0, Aktarilacakveriler, sayac, 1);
+        bufferINT.clear();
+        sayac += 1;
+        bufferINT.putInt(paketSayac);
+        System.arraycopy(bufferINT.array(), 0, Aktarilacakveriler, sayac, 1);
+        bufferINT.clear();
+        sayac += 1;
+        buffer.putFloat(basincIrtifa);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(roketGpsIrtifa);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(roketEnlem);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(roketBoylam);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(gorevYukuGpsIrtifa);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(gorevYukuEnlem);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(gorevYukuBoylam);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+
+        // !! bu noktada kademe gps irtifa vs verilerini geçiyoruz bunlar zorlu görev kategorisinde olan değerler!!
+        // Burada sabit değerler olacak.
+        sayac+=12;
+
+
+        buffer.putFloat(jiroskopX);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(jiroskopY);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(jiroskopZ);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(ivmeX);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(ivmeY);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(ivmeZ);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        buffer.putFloat(aci);
+        System.arraycopy(buffer.array(), 0, Aktarilacakveriler, sayac, 4);
+        sayac += 4;
+        buffer.clear();
+        bufferINT.putInt(durum);
+        System.arraycopy(bufferINT.array(), 0, Aktarilacakveriler, sayac, 1);
+        bufferINT.clear();
+        sayac += 1;
+        bufferINT.putInt(crc);
+        System.arraycopy(bufferINT.array(), 0, Aktarilacakveriler, sayac, 1);
+        bufferINT.clear();
+        sayac += 1;
     }
 
-    public static void setJiroskopZ(double jiroskopZ) {
-        veriler.jiroskopZ = jiroskopZ;
-    }
 
-    public static double getIvmeX() {
-        return ivmeX;
-    }
-
-    public static void setIvmeX(double ivmeX) {
-        veriler.ivmeX = ivmeX;
-    }
-
-    public static double getIvmeY() {
-        return ivmeY;
-    }
-
-    public static void setIvmeY(double ivmeY) {
-        veriler.ivmeY = ivmeY;
-    }
-
-    public static double getIvmeZ() {
-        return ivmeZ;
-    }
-
-    public static void setIvmeZ(double ivmeZ) {
-        veriler.ivmeZ = ivmeZ;
-    }
-
-    public static double getAci() {
-        return aci;
-    }
-
-    public static void setAci(double aci) {
-        veriler.aci = aci;
-    }
-
-    public static double getGorevYukuBoylam() {
-        return gorevYukuBoylam;
-    }
-
-    public static void setGorevYukuBoylam(double gorevYukuBoylam) {
-        veriler.gorevYukuBoylam = gorevYukuBoylam;
-    }
-
-    public static double getGorevYukuEnlem() {
-        return gorevYukuEnlem;
-    }
-
-    public static void setGorevYukuEnlem(double gorevYukuEnlem) {
-        veriler.gorevYukuEnlem = gorevYukuEnlem;
-    }
-
-    public static double getGorevYukuGpsIrtifa() {
-        return gorevYukuGpsIrtifa;
-    }
-
-    public static void setGorevYukuGpsIrtifa(double gorevYukuGpsIrtifa) {
-        veriler.gorevYukuGpsIrtifa = gorevYukuGpsIrtifa;
-    }
-
-    public static double getRoketBoylam() {
-        return roketBoylam;
-    }
-
-    public static void setRoketBoylam(double roketBoylam) {
-        veriler.roketBoylam = roketBoylam;
-    }
-
-    public static double getRoketEnlem() {
-        return roketEnlem;
-    }
-
-    public static void setRoketEnlem(double roketEnlem) {
-        veriler.roketEnlem = roketEnlem;
-    }
-
-    public static double getRoketGpsIrtifa() {
-        return roketGpsIrtifa;
-    }
-
-    public static void setRoketGpsIrtifa(double roketGpsIrtifa) {
-        veriler.roketGpsIrtifa = roketGpsIrtifa;
-    }
 }
